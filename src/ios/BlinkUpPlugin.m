69d685226ed2cb469f1deb855601c5c3f4e1c527
Shrikant Kakaraparthi <shrikant.k@seventablets.com>
2018-09-28 14:05:46 -0500

    NSLog api key

--- src/ios/BlinkUpPlugin.m
+++ src/ios/BlinkUpPlugin.m
@@ -48,7 +48,7 @@ typedef NS_ENUM(NSInteger, InvokeBlinkupArguments) {
             return;
         }
 
-        NSLog(@"startBlinkUp. timeoutMs? %ld", (long)_timeoutMs);
+        NSLog(@"startBlinkUp. timeoutMs? %ld apiKey? %@", (long)_timeoutMs, _apiKey);
 
         [self navigateToBlinkUpView];
     }];
